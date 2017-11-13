4c6ec11118f2f4b391bc4887cb7ee33865c4a6ec
Greg Holmes <greg.holmes@housesimple.com>
2017-11-13 16:48:19 +0000

    Added Auth0ResourceOwner

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Http\HttplugBundle\HttplugBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         ];
 
