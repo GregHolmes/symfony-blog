5176f6f4a3ee8d71a94bc43eaa5a08db3c26e687
Greg Holmes <greg.holmes@housesimple.com>
2017-11-24 15:17:19 +0000

    Added flow for part 1 of tutorial. Needs further clean up and testing

---
+++ part-1/src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Author;
+use AppBundle\Form\AuthorFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends Controller
+{
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $blogPostRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('AppBundle:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('AppBundle:Author');
+    }
+
+    /**
+     * @Route("/admin/author/create", name="author_create")
+     */
+    public function createAuthorAction(Request $request)
+    {
+        // Check whether user already has an author.
+        if ($this->authorRepository->findOneByUsername($this->getUser()->getUserName())) {
+            // Redirect to dashboard.
+            $this->addFlash('error','Unable to create author, author already exists!');
+
+            return $this->redirectToRoute('entries');
+        }
+
+        $author = new Author();
+        $author->setUsername($this->getUser()->getUserName());
+
+        $form = $this->createForm(AuthorFormType::class, $author);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->entityManager->persist($author);
+            $this->entityManager->flush($author);
+
+            $request->getSession()->set('user_is_author', true);
+            $this->addFlash('success','Congratulations! You are now an author.');
+
+            return $this->redirectToRoute('entries');
+        }
+
+        return $this->render('AppBundle:Admin:create_author.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+}
